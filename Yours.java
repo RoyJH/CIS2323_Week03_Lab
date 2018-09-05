d90a0fa4bacf466885b2175defd9cba708806a43
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-05 18:56:53 -0500

    Others

--- Yours.java
+++
@@ -1,5 +0,0 @@
-public class Yours {
-	public static void print(){
-		System.out.println("This is yours!");
-	}
-}
---
+++ mystuff/Yours.java
@@ -0,0 +1,13 @@
+package mystuff;
+
+public class Yours {
+	public static void print(){
+		System.out.println("This is yours!");
+	}
+}
+
+// public class Others {
+	// public static void print(){
+	// System.out.println("This is others!");
+	// }
+// }
